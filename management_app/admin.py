89b13326cb0ae94bf9e87d1cc9c8d268a228c222
Openiuk <openyusha@gmail.com>
2024-06-04 10:49:44 +0200

    register models to admin panel

--- management_app/admin.py
+++ management_app/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from management_app.models import Project, Task, Tag
 
-# Register your models here.
+#
+# Настройте отображение моделей Project, Task, Tag в админ-панели. Реализуйте следующие возможности:
+#
+# Поиск по названию задачи для модели Task.
+# Поиск по названию проекта для модели Project.
+# У модели Task в Админ-панели должны отображаться поля:
+# Название задачи
+# Проект
+# Статус
+# Приоритетность
+# Дата создания
+# Дата сдачи задачи (due_date)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'project', 'status', 'priority', 'created_date', 'due_date']
+
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    ...
