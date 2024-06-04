5664ac74d000b19c29f193a5069dc566600146d3
Openiuk <openyusha@gmail.com>
2024-06-04 11:42:16 +0200

    add model project_file

--- management_app/models/project.py
+++ management_app/models/project.py
@@ -9,6 +9,29 @@ class Project(models.Model):
     name = models.CharField(unique=True, max_length=100)
     description = models.TextField()
     date_of_creation = models.DateTimeField(auto_now_add=True)
+    project_files = models.ManyToManyField('ProjectFile', related_name='ProjectFile')
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ['-name']
+        verbose_name_plural = 'Projects'
+        unique_together = (('name', 'description'),)
+
+
+class ProjectFile(models.Model):
+    file_name = models.CharField(max_length=120)
+    file_path = models.FileField(upload_to='projects/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.file_name
+
+
+    class Meta:
+
+        ordering = ['-created_at']
+        verbose_name = 'Project File'
+        verbose_name_plural = 'Project Files'
+
