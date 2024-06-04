706faf0379cc4d74bce26af27130e375ee198f26
vPareiko <parieiko.vlad@gmail.com>
2024-06-04 18:12:31 +0400

    Add new property field, and new relation field 'files'

--- management_app/models/project.py
+++ management_app/models/project.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 
-# Название проекта: строковое, уникальное
-# Описание проекта: строковое, большое поле, обязательно к заполнению
-# Дата создания проекта: должна проставляться автоматически при создании
-
-
 class Project(models.Model):
     name = models.CharField(unique=True, max_length=100)
     description = models.TextField()
     date_of_creation = models.DateTimeField(auto_now_add=True)
     project_files = models.ManyToManyField('ProjectFile', related_name='project_files')
+    files = models.ManyToManyField('ProjectFile', related_name='files')
+
+    @property
+    def count_of_files(self):
+        return self.files.count()
 
     def __str__(self):
         return self.name
@@ -19,7 +19,6 @@ class Project(models.Model):
         ordering = ['-name']
         verbose_name = 'Project'
         verbose_name_plural = 'Projects'
-        # unique_together = ('name', 'description')
 
 
 class ProjectFile(models.Model):
