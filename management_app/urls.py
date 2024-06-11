2440d7ccc928ac4c5e8b5e2a5c8faf4a3cb82a93
Dimm <slim.slk@gmail.com>
2024-06-11 11:59:27 +0200

    add new route to create a new tag

--- management_app/urls.py
+++ management_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from management_app.views import get_all_projects
+from management_app.views import get_all_projects, create_new_tag
 
 urlpatterns = [
-    path('projects/', get_all_projects)
+    path('projects/', get_all_projects),
+    path('tags/create/', create_new_tag)
 ]
--- management_app/views.py
+++ management_app/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 
 from management_app.models import Project
 from management_app.serializers.projects import AllProjectsSerializer
+from management_app.serializers.tags import TagSerializer
 
 
 @api_view(["GET"])
@@ -15,3 +16,16 @@ def get_all_projects(request: Request) -> Response:
     serialize = AllProjectsSerializer(projects, many=True)
 
     return Response(data=serialize.data, status=status.HTTP_200_OK)
+
+
+@api_view(["POST"])
+def create_new_tag(request: Request) -> Response:
+    tag_data = request.data
+    serializer = TagSerializer(data=tag_data)
+
+    if not serializer.is_valid():
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    serializer.save()
+
+    return Response(data=serializer.data, status=status.HTTP_201_CREATED)
