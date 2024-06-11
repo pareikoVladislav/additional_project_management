b253baec29018e36ad1795fa08c473c1c84cd36b
Openiuk <openyusha@gmail.com>
2024-06-11 12:16:36 +0200

    add new endpoint get tag by id

--- management_app/urls.py
+++ management_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from management_app.views import get_all_projects, get_all_tags
+from management_app.views import (get_all_projects,
+                                  get_all_tags,
+                                  get_tag_by_id
+                                  )
 
 urlpatterns = [
     path('projects/', get_all_projects),
     path('tags/', get_all_tags),
+    path('tags/<int:pk>/', get_tag_by_id)
 ]
